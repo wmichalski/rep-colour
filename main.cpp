c26597fce153e19199a7856239b5c3b27d38a020
wmichalski <wiktor.michalski.2@gmail.com>
2019-01-26 00:02:51 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -39,22 +39,33 @@ rgb_t get_colour(int x, int y)
 int main()
 {
     int rep, posts;
-    int srch, srcw;
+    int src_h, src_w, des_h, des_w;
     double wsp;
     cout << "Insert height and width of the image: ";
-    cin >> srch >> srcw;
+    cin >> src_h >> src_w;
+
+    cout << "Insert desired height and width of the image: ";
+    cin >> des_h >> des_w;
 
     cout << "Insert rep and posts you want to highlight: ";
     cin >> rep >> posts;
 
-    bitmap_image image(srcw, srch);
+    bitmap_image image(des_w, des_h);
+
+    double scale_w, scale_h;
+
+    scale_w = double(src_w/des_w);
+    scale_h = double(src_h/des_h);
+
+    int scaled_rep = round(rep/scale_h);
+    int scaled_posts = round(posts/scale_w);
 
     for (unsigned int y = 0; y < image.height(); ++y)
     {
         for (unsigned int x = 0; x < image.width(); ++x)
         {
-            image.set_pixel(x, y, get_colour(x, y));
-            if ((x == posts || y == rep) && !(x == posts && y == rep))
+            image.set_pixel(x, y, get_colour(scale_w*x, scale_h*y));
+            if ((x == scaled_posts || y == scaled_rep) && !(x == scaled_posts && y == scaled_rep))
                 image.set_pixel(x, y, 00,00,00);
         }
     }
@@ -63,6 +74,10 @@ int main()
     cout << "\nYour wsp: " << wsp;
     rgb_t user_clr = get_colour(posts, rep);
     cout << "\nYour colour: #" << hex << setfill('0') << setw(2) << (int)user_clr.red << std::setw(2) << (int)user_clr.green << std::setw(2) <<(int)user_clr.blue;
+    user_clr = image.get_pixel(scaled_posts, scaled_rep);
+    cout << "\nColour from map: #" << hex << setfill('0') << setw(2) << (int)user_clr.red << std::setw(2) << (int)user_clr.green << std::setw(2) <<(int)user_clr.blue;
+
+
 
     image.save_image("output.bmp");
 }
